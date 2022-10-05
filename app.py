43704a156dcacff7cd82e0bf1d49134c370ae88c
sunshay <sunshayina@outlook.com>
2022-10-04 21:33:16 -0700

    commit

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 from flask import Flask, render_template, request,flash
 
-app = Flask(__name__)  #initialise app ( create a class)
+app = Flask(__name__) #initialise app ( create a class)
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-    if __name__ == "__main__":
-        app.run(debug=True)
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
         
 
